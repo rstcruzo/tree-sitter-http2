e1c1bf9e29574dc1970d2dfb0cd372591daf76ed
Rodrigo Santa Cruz <rstcruzo@gmail.com>
2023-09-26 19:29:56 -0500

    Add trailing comma.

--- grammar.js
+++ grammar.js
@@ -13,14 +13,14 @@ module.exports = grammar({
                     $.method_url,
                     $._new_line,
                     repeat(seq($.header, $._new_line)),
-                    optional($.json_body)
-                )
+                    optional($.json_body),
+                ),
             ),
         method_url: ($) => seq($.method, $.url),
         response: ($) =>
             prec.left(
                 2,
-                seq($.status_line, repeat(seq($.header, $._new_line)))
+                seq($.status_line, repeat(seq($.header, $._new_line))),
             ),
         status_line: ($) =>
             seq($.http_version, $.status_code, $.reason_phrase, $._new_line),
@@ -35,23 +35,23 @@ module.exports = grammar({
                     "header_value",
                     choice(
                         seq($._kill_leading_whitespace, $.rest_of_line),
-                        seq($._kill_leading_whitespace, $.variable_ref)
-                    )
-                )
+                        seq($._kill_leading_whitespace, $.variable_ref),
+                    ),
+                ),
             ),
         method: ($) =>
             choice(
                 /(OPTIONS|GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT|PATCH)/,
-                $.variable_ref
+                $.variable_ref,
             ),
         url: ($) =>
             seq(
                 choice(
                     seq($.scheme, "://", choice($.domain, $.variable_ref)),
-                    $.variable_ref
+                    $.variable_ref,
                 ),
                 optional($.path),
-                optional(seq("?", optional($.query_params)))
+                optional(seq("?", optional($.query_params))),
             ),
         scheme: (_) =>
             /(about|acct|arcp|cap|cid|coap+tcp|coap+ws|coaps+tcp|coaps+ws|data|dns|example|file|ftp|geo|h323|http|https|im|info|ipp|mailto|mid|ni|nih|payto|pkcs11|pres|reload|secret-token|session|sms|tag|telnet|urn|ws|wss)/,
@@ -60,9 +60,9 @@ module.exports = grammar({
             choice(
                 seq(
                     repeat1(seq("/", choice($._identifier, $.variable_ref))),
-                    optional("/") // Trailing slash
+                    optional("/"), // Trailing slash
                 ),
-                "/" // Path is only the trailing slash
+                "/", // Path is only the trailing slash
             ),
         query_params: ($) =>
             seq($.query_param, repeat(seq("&", $.query_param))),
@@ -70,7 +70,7 @@ module.exports = grammar({
             seq(
                 field("parameter_name", choice($.identifier, $.variable_ref)),
                 "=",
-                field("parameter_value", choice($.identifier, $.variable_ref))
+                field("parameter_value", choice($.identifier, $.variable_ref)),
             ),
         json_body: ($) =>
             choice(seq("{", optional($.key_value_list), "}"), $.list),
@@ -78,14 +78,14 @@ module.exports = grammar({
             seq(
                 $.key_value,
                 repeat(seq(",", optional($._new_line), $.key_value)),
-                optional($._new_line)
+                optional($._new_line),
             ),
         key_value: ($) => seq($.key, ":", $.value),
         key: ($) => seq('"', choice($.identifier, $.variable_ref), '"'),
         value: ($) =>
             prec(
                 2,
-                choice($.string, $.number, $.json_body, $.variable_ref, $.list)
+                choice($.string, $.number, $.json_body, $.variable_ref, $.list),
             ),
         list: ($) => seq("[", optional($.list_values), "]"),
         list_values: ($) => seq($.value, repeat(seq(",", $.value))),
@@ -96,9 +96,9 @@ module.exports = grammar({
                 "=",
                 field(
                     "variable_value",
-                    seq($._kill_leading_whitespace, $.rest_of_line)
+                    seq($._kill_leading_whitespace, $.rest_of_line),
                 ),
-                $._new_line
+                $._new_line,
             ),
         variable_ref: () => token(prec(2, seq("{{", /[A-Za-z_\.\d]*/, "}}"))),
         identifier: ($) => $._identifier,
